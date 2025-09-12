83b5253a1add7046f2712954de5c713d641d7b47
wampadu <50532644+wampadu@users.noreply.github.com>
2025-09-11 20:13:43 -0400

    Update events_emailer.py

--- events_emailer.py
+++ events_emailer.py
@@ -15,10 +15,10 @@ from email.mime.application import MIMEApplication
 # === Calculate Upcoming Friday–Sunday Dates ===
 def get_upcoming_weekend_dates():
     today = datetime.today()
-    days_until_friday = (4 - today.weekday()) % 7
-    friday = today + timedelta(days=days_until_friday + 0)
-    return [today + timedelta(days=3), today + timedelta(days=10)]
+    #days_until_friday = (4 - today.weekday()) % 7
+    #friday = today + timedelta(days=days_until_friday + 0)
     #return [friday, friday + timedelta(days=1), friday + timedelta(days=2)]
+    return [today + timedelta(days=10), today + timedelta(days=17)]
 
 # === HTML Output ===
 def generate_html(events):
